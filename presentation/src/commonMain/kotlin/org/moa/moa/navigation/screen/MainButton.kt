3a509fe1a013beef11c5e63df6ced622e2550de4
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-08-15 17:41:24 +0900

    feat: add BottomBar at MOAApp

---
+++ presentation/src/commonMain/kotlin/org/moa/moa/navigation/screen/MainButton.kt
@@ -0,0 +1,3 @@
+package org.moa.moa.navigation.screen
+
+data object MainButton : Screen("main_button")
---
+++ presentation/src/commonMain/kotlin/org/moa/moa/navigation/screen/Sign.kt
@@ -0,0 +1,5 @@
+package org.moa.moa.navigation.screen
+
+data object OnBoarding : Screen("onboarding")
+data object SignIn : Screen("sign_in")
+data object SignUp : Screen("sign_up")
