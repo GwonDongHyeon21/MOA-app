2b8e85dfe5953adc09140a8d235d5f4c354ed8c7
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-10-24 07:50:59 +0900

    feat: SignUpViewModel 수동 주입 형태로 수정

--- composeApp/src/iosMain/kotlin/org/moa/moa/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/moa/moa/MainViewController.kt
@@ -1,16 +1,40 @@
 package org.moa.moa
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.moa.data.BuildConfig
 import com.moa.data.di.dataModule
+import com.moa.data.network.ApiService
+import com.moa.data.repositoryimpl.SignRepositoryImpl
 import com.moa.domain.di.domainModule
+import com.moa.domain.usecase.sign.GetUserInfo
+import com.moa.domain.usecase.sign.GoogleSignUp
+import com.moa.domain.usecase.sign.SignUseCase
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import org.moa.moa.di.platformModule
 import org.moa.moa.di.presentationModule
+import org.moa.moa.presentation.sign.SignUpViewModel
+import org.moa.moa.presentation.sign.platform.GoogleAuthCodeProvider
+import org.moa.moa.presentation.sign.platform.IOSViewControllerHolder
 
 fun MainViewController() = ComposeUIViewController {
     initKoin()
-    MOAApp()
+
+    val viewModel = SignUpViewModel(
+        signUseCase = SignUseCase(
+            getUserInfo = GetUserInfo(
+                SignRepositoryImpl(ApiService())
+            ),
+            googleSignUp = GoogleSignUp(
+                SignRepositoryImpl(ApiService())
+            )
+        ),
+        googleAuthCodeProvider = GoogleAuthCodeProvider(
+            IOSViewControllerHolder(),
+            BuildConfig.GOOGLE_SERVER_CLIENT_ID
+        )
+    )
+    MOAApp(viewModel)
 }
 
 fun initKoin() {
